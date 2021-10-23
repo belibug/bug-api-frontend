892b30d38a3073ceed6c7634f45921cd4e78db2d
Manjunath Beli <belibug@outlook.com>
2021-10-22 22:16:32 -0400

    good progress

--- src/views/InputFields.vue
+++ src/views/InputFields.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container">
     <div class="section">
-      <div class="input-box">
-        <div class="header-bar" @click="showNoteField = !showNoteField">
+      <div class="menu-header">
+        <div class="header-bar" @click="showView(1)">
           <h2>Notes</h2>
         </div>
+        <div class="header-bar" @click="showView(2)">
+          <h2>Tracker</h2>
+        </div>
+      </div>
+      <div class="menu-box">
         <div class="input-form" v-if="showNoteField">
           <form>
             <div class="form-control">
@@ -18,18 +23,37 @@
             </div>
           </form>
         </div>
-      </div>
-      <div class="input-box">
-        <div class="header-bar" @click="showTrackerField = !showTrackerField">
-          <h2>Tracker</h2>
-        </div>
         <div class="input-form" v-if="showTrackerField">
           <form>
             <div class="form-control">
-              <input type="text" name="title" placeholder="title" required />
+              <input
+                type="text"
+                name="name"
+                placeholder="activity name"
+                required
+              />
             </div>
             <div class="form-control">
-              <textarea type="text" name="body" placeholder="content" />
+              <input
+                type="text"
+                name="category"
+                placeholder="category"
+                required
+              />
+            </div>
+            <div class="form-control">
+              <input
+                type="number"
+                name="duration"
+                placeholder="duration in minutes"
+                required
+              />
+              <div class="form-control">
+                <input type="text" name="comments" placeholder="comments" />
+              </div>
+            </div>
+            <div class="form-control">
+              <input type="datetime-local" name="timestamp" />
             </div>
             <div class="submit-btn">
               <input type="submit" class="btn btn-primary" value="Add" />
@@ -46,9 +70,15 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return {
-      showNoteField: false as boolean,
+      showNoteField: true as boolean,
       showTrackerField: false as boolean,
     };
+  },
+  methods: {
+    showView(num: number) {
+      this.showNoteField = num == 1 ? true : false;
+      this.showTrackerField = num == 2 ? true : false;
+    },
   },
 });
 </script>
@@ -56,51 +86,31 @@ export default defineComponent({
 <style scoped>
 .section {
   display: flex;
-  flex-direction: column;
+
   align-content: center;
   justify-content: center;
 }
-.input-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 20px;
+
+.menu-header {
+  height: 400px;
+  background-color: cadetblue;
+  margin: 5px;
+  padding: 10px;
+  width: 20%;
 }
-.input-form {
-  padding: 20px;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
-  border: 1px black solid;
-  border-top: 0px;
-  width: 800px;
-  /* width: 30rem; */
-  /* display: none; */
+.menu-box {
+  padding: 10px;
+  margin: 5px;
+  height: 400px;
+  width: 40%;
+  background-color: coral;
 }
-.form-control input[type="text"],
-.form-control input[type="datetime-local"],
-.form-control input[type="number"],
-.form-control textarea {
-  border: 0;
-  border-bottom: 1px solid #000;
-  width: 95%;
-  padding: 3px;
-  font-size: 1.2em;
-}
-.form-control input:focus,
-.form-control textarea:focus {
-  outline: none;
-}
-textarea {
-  height: 4em;
-}
+
 .header-bar {
-  background-color: burlywood;
-  padding: 2px 30px;
-  width: 80%;
-  border-radius: 15px;
-  user-select: none;
+  background-color: blanchedalmond;
+  padding: 10px;
+  margin: 5px;
+  border-radius: 10px;
 }
 
 @media only screen and (max-width: 600px) {
